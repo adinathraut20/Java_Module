bc3183a3ea2e3a091f8afaf99496bb5a30406075
Adinath <adinathraut20@gmail.com>
2020-09-21 22:14:49 +0530

    Restruct

---
+++ Assignment/Assignment1-30/src/q37.java
@@ -0,0 +1,33 @@
+/* 37. Create a class Person with properties (name and age) with following features. 
+a. Default age of person should be 18.
+b. A person object can be initialized with name and age.
+c. Method to display name and age of person
+Create another class PersonDemo ( main class ) that demonstrates the functionalities of Person class by creating Person object and calling methods.
+*/
+
+
+class Person{
+	int age;
+	String name;
+	Person(){
+		this.age = 18;
+		this.name = "";
+	}
+
+	Person(int age,String name){
+		this.age = age;
+		this.name = name;
+	}
+	void display (){
+		System.out.println("Name : " + this.name + "   Age : " + this.age);
+	}
+}
+
+class PersonDemo{
+	public static void main(String []args){
+		Person p1 = new Person();
+		Person p2 = new Person(20,"Adi");
+		p1.display();
+		p2.display();
+	}
+}
