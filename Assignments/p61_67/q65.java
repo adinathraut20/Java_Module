a7d268585e09ee79c328bb6e9e7021b68efbdb33
adinathraut20 <adinathraut20@gmail.com>
2020-10-06 23:54:09 +0530

    Atlast

--- Assignments/p61_67/q65.java
+++ Assignments/p61_67/q65.java
@@ -5,11 +5,31 @@
 
 package p61_67;
 
-public class q65 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	import java.awt.image.BufferedImage;
+	import java.io.ByteArrayInputStream;
+	import java.io.ByteArrayOutputStream;
+	import java.io.File;
+	import java.io.IOException;
+	import javax.imageio.ImageIO;
+	import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+	 
+	 
+	public class q65 {
+		public static void main(String[] args) throws IOException{
+			String dirName="C:\\Users\\Admin\\OneDrive\\Pictures\\Screenshots";
+			ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
+			BufferedImage img=ImageIO.read(new File(dirName,"adi.png"));
+			ImageIO.write(img, "jpg", baos);
+			baos.flush();
+	 
+			String base64String=Base64.encode(baos.toByteArray());
+			baos.close();
+	 
+			byte[] bytearray = Base64.decode(base64String);
+	 
+			BufferedImage imag=ImageIO.read(new ByteArrayInputStream(bytearray));
+			ImageIO.write(imag, "jpg", new File(dirName,"snap.jpg"));
+		}
 	}
-
-}
